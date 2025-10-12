48a9c79cd9bb8ab43cd9db1baca6c96c8e4859a4
SunnyKapur <kumarsunnykumar678@gmail.com>
2025-10-12 21:52:47 +0530

    Frontend create

---
+++ frontend/src/components/MoodSongs.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./MoodSongs.css";
+const MoodSongs = () => {
+  const [Songs, setSongs] = useState([
+    {
+      title: "test_title",
+      artist: "test_artist",
+      url: "test_url",
+    },
+    {
+      title: "test_title",
+      artist: "test_artist",
+      url: "test_url",
+    },
+    {
+      title: "test_title",
+      artist: "test_artist",
+      url: "test_url",
+    },
+  ]);
+
+  return (
+    <div className="mood-song">
+      <h2>Recommened Songs</h2>
+
+      {Songs.map((song, index) => (
+        <div className="song" key={index}>
+          <div className="title">
+            <h3>{song.title}</h3>
+            <p>{song.artist}</p>
+          </div>
+          <div className="play-pause-button">
+            <i className="ri-pause-line"></i>
+            <i className="ri-play-circle-fill"></i>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MoodSongs;
